4ea99b9956428838ddabec324ff27dc0bf1e8573
arthurarabelo <arthurrabelo2015@hotmail.com>
2025-03-24 21:48:34 -0300

    creating status circle component to use it in the index page

---
+++ front/src/components/StatusCircle.vue
@@ -0,0 +1,25 @@
+<style lang="scss">
+@import '../css/status.circle.scss';
+</style>
+
+<template>
+  <div class="status-container">
+    <span :class="[status ? activeStatusClass : inactiveStatusClass]"></span>
+    <span class="status-text">{{ text }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const activeStatusClass = ref('green-status-circle');
+const inactiveStatusClass = ref('red-status-circle');
+
+defineProps({
+  text: String,
+  status: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
