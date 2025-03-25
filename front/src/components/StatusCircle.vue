0b3bb08d6b1cbf175712da29b2b41b4f603c8fb9
arthurarabelo <arthurrabelo2015@hotmail.com>
2025-03-25 08:27:24 -0300

    Updating the status circle component with the reviewed changes

--- front/src/components/StatusCircle.vue
+++ front/src/components/StatusCircle.vue
@@ -4,22 +4,18 @@
 
 <template>
   <div class="status-container">
-    <span :class="[status ? activeStatusClass : inactiveStatusClass]"></span>
-    <span class="status-text">{{ text }}</span>
+    <span :class="['circle', status]"></span>
+    <span v-if="text" class="status-text">{{ text }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const activeStatusClass = ref('green-status-circle');
-const inactiveStatusClass = ref('red-status-circle');
-
 defineProps({
   text: String,
   status: {
-    type: Boolean,
+    type: String,
     required: true,
+    validator: (value: string) => ['active', 'inactive'].includes(value),
   },
 });
 </script>
