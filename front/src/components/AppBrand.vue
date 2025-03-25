4a99cf4b4bd0a7572668e534e541416892102d1e
arthurarabelo <arthurrabelo2015@hotmail.com>
2025-03-25 10:32:03 -0300

    front initial adjustments related to issue#9

---
+++ front/src/components/AppBrand.vue
@@ -0,0 +1,10 @@
+<style lang="scss">
+@import '../css/brand.scss';
+</style>
+
+<template>
+  <div class="bg-dark-blue">
+    <span class="text-white"> ICEx </span>
+    <span class="text-light-blue"> HUB </span>
+  </div>
+</template>
