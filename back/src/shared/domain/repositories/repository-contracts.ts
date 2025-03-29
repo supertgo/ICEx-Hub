8a443599a8c31318e5fcbe4000947526b6890fa9
davi <davi.araujo@onfly.com.br>
2025-03-29 10:16:06 -0300

    adjust repository to return created entity

--- back/src/shared/domain/repositories/repository-contracts.ts
+++ back/src/shared/domain/repositories/repository-contracts.ts
@@ -1,7 +1,7 @@
 import { Entity } from '@/shared/domain/entities/entity';
 
 export interface RepositoryInterface<E extends Entity> {
-  insert(entity: E): Promise<void>;
+  insert(entity: E): Promise<E>;
 
   findById(id: string): Promise<E>;
 
