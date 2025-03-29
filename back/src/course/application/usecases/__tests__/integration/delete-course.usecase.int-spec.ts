df80ed50a46de54be939a4772b840ea4bfad2302
davi <davi.araujo@onfly.com.br>
2025-03-29 10:17:47 -0300

    implement delete course usecase spec

--- back/src/course/application/usecases/__tests__/integration/delete-course.usecase.int-spec.ts
+++ back/src/course/application/usecases/__tests__/integration/delete-course.usecase.int-spec.ts
@@ -4,6 +4,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { setUpPrismaTest } from '@/shared/infrastructure/database/prisma/testing/set-up-prisma-test';
 import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
 import { DeleteCourseUsecase } from '@/course/application/usecases/delete-course.usecase';
+import { CourseWithIdNotFoundError } from '@/course/infrastructure/Errors/course-with-id-not-found-error';
+import { faker } from '@faker-js/faker';
+import {
+  CoursePrismaTestingHelper,
+} from '@/course/infrastructure/database/prisma/testing/course-prisma.testing-helper';
 
 describe('Delete Course usecase integration tests', () => {
   const prismaService = new PrismaClient();
@@ -31,8 +36,20 @@ describe('Delete Course usecase integration tests', () => {
     await module.close();
   });
 
-  it('should throw error when course not found', () => { });
+  it('should throw error when course not found', () => {
+    const id = faker.string.uuid();
+    expect(() => sut.execute({ id })).rejects.toThrowError(
+      new CourseWithIdNotFoundError(id),
+    );
+  });
 
   it('should delete a course', async () => {
+    const course = await CoursePrismaTestingHelper.createCourse(prismaService);
+
+    await sut.execute({ id: course.id });
+
+    const count = await prismaService.course.count();
+
+    expect(count).toBe(0);
   });
 });
