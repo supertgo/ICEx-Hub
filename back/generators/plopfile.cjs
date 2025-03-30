22a24c448178c3b1772aec06cefc91f761bc5aa4
supertgo <thiagoroberto2003@gmail.com>
2025-03-30 18:28:00 -0300

    chore(generator): add data and fake builder

--- back/generators/plopfile.cjs
+++ back/generators/plopfile.cjs
@@ -81,6 +81,25 @@ module.exports = (plop) => {
           },
         );
 
+        //DataBuilder and FakeBuilder
+        actions.push(
+          {
+            type: 'add',
+            path: `${srcFolder}/{{dashCase name}}/domain/testing/helper/{{dashCase name}}-data-builder.ts`,
+            templateFile: `templates/entity/domain/testing/helper/entity-data-builder.ts.hbs`,
+          },
+          {
+            type: 'add',
+            path: `${srcFolder}/{{dashCase name}}/domain/fake-builder/{{dashCase name}}-fake.builder.ts`,
+            templateFile: `templates/entity/domain/fake-builder/entity-fake.builder.ts.hbs`,
+          },
+          {
+            type: 'add',
+            path: `${srcFolder}/{{dashCase name}}/domain/fake-builder/__tests__/unit/{{dashCase name}}-fake.builder.spec.ts`,
+            templateFile: `templates/entity/domain/fake-builder/entity-fake.builder.spec.ts.hbs`,
+          },
+        );
+
         //Repositories
         actions.push(
           {
