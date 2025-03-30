6849e24bdf387382d159d127701bad4f398c88a9
supertgo <thiagoroberto2003@gmail.com>
2025-03-30 19:38:29 -0300

    feat(schedule): add entity

---
+++ back/src/schedule/domain/entities/__tests__/integration/schedule.entity.int-spec.ts
@@ -0,0 +1,61 @@
+import {
+  ScheduleEntity,
+  ScheduleProps,
+} from '@/schedule/domain/entities/schedule.entity';
+import {
+  DayPatternEnum,
+  TimeSlotEnum,
+} from '@/schedule/domain/schedule.constants';
+import { ScheduleDataBuilder } from '@/schedule/domain/testing/helper/schedule-data-builder';
+import { EntityValidationError } from '@/shared/domain/errors/validation-errors';
+
+describe('Schedule entity unit tests', () => {
+  beforeEach(() => {
+    ScheduleEntity.validate = jest.fn();
+  });
+
+  it('should throw error with classroomId code', () => {
+    const props: ScheduleProps = {
+      ...ScheduleDataBuilder({}),
+      classroomId: 'test',
+    };
+
+    expect(() => new ScheduleEntity(props)).toThrow(EntityValidationError);
+  });
+
+  it('should throw error with classroomId', () => {
+    const props: ScheduleProps = {
+      ...ScheduleDataBuilder({}),
+      classroomId: 'test',
+    };
+
+    expect(() => new ScheduleEntity(props)).toThrow(EntityValidationError);
+  });
+
+  it('should throw error with disciplineId', () => {
+    const props: ScheduleProps = {
+      ...ScheduleDataBuilder({}),
+      disciplineId: 'test',
+    };
+
+    expect(() => new ScheduleEntity(props)).toThrow(EntityValidationError);
+  });
+
+  it('should throw error with invalid dayPattern', () => {
+    const props: ScheduleProps = {
+      ...ScheduleDataBuilder({}),
+      dayPattern: undefined as DayPatternEnum,
+    };
+
+    expect(() => new ScheduleEntity(props)).toThrow(EntityValidationError);
+  });
+
+  it('should throw error with invalid timeSlot', () => {
+    const props: ScheduleProps = {
+      ...ScheduleDataBuilder({}),
+      timeSlot: undefined as TimeSlotEnum,
+    };
+
+    expect(() => new ScheduleEntity(props)).toThrow(EntityValidationError);
+  });
+});
---
+++ back/src/schedule/domain/entities/schedule.entity.ts
@@ -0,0 +1,98 @@
+import { Entity } from '@/shared/domain/entities/entity';
+import { ScheduleValidatorFactory } from '@/schedule/domain/validators/schedule.validator';
+import { EntityValidationError } from '@/shared/domain/errors/validation-errors';
+import { ClassroomEntity } from '@/classroom/domain/entities/classroom.entity';
+import { DayPatternEnum, TimeSlotEnum } from '../schedule.constants';
+
+type DisciplineProps = {
+  name: string;
+  code: string;
+  courseId: string;
+  coursePeriodId: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type ScheduleProps = {
+  disciplineId: string;
+  discipline?: DisciplineProps;
+  classroomId: string;
+  classroom?: ClassroomEntity | null;
+  dayPattern: DayPatternEnum;
+  timeSlot: TimeSlotEnum;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export class ScheduleEntity extends Entity<ScheduleProps> {
+  constructor(
+    public readonly props: ScheduleProps,
+    id?: string,
+  ) {
+    ScheduleEntity.validate(props);
+    super(props, id);
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+
+  get disciplineId() {
+    return this.props.disciplineId;
+  }
+
+  set disciplineId(disciplineId: string) {
+    this.props.disciplineId = disciplineId;
+  }
+
+  get discipline() {
+    return this.props.discipline;
+  }
+
+  get classroomId() {
+    return this.props.classroomId;
+  }
+
+  set classroomId(classroomId: string) {
+    this.props.classroomId = classroomId;
+  }
+
+  get dayPattern() {
+    return this.props.dayPattern;
+  }
+
+  set dayPattern(dayPattern: DayPatternEnum) {
+    this.props.dayPattern = dayPattern;
+  }
+
+  get timeSlot() {
+    return this.props.timeSlot;
+  }
+
+  set timeSlot(timeSlot: TimeSlotEnum) {
+    this.props.timeSlot = timeSlot;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  private set createdAt(value: Date) {
+    this.props.createdAt = value;
+  }
+
+  get updatedAt(): Date | undefined {
+    return this.props.updatedAt;
+  }
+
+  private set updatedAt(value: Date) {
+    this.props.updatedAt = value;
+  }
+
+  static validate(props: ScheduleProps) {
+    console.log(props, 'props');
+    const validator = ScheduleValidatorFactory.create();
+    const isValid = validator.validate(props);
+
+    if (!isValid) {
+      throw new EntityValidationError(validator.errors);
+    }
+  }
+}
