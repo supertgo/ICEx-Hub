64a63e0b966455283dd732cf17727a73ef16f71a
davi <davi.araujo@onfly.com.br>
2025-03-30 07:38:58 -0300

    ad course fake builder and its tests

--- back/src/course/domain/entities/course.entity.ts
+++ back/src/course/domain/entities/course.entity.ts
@@ -23,6 +23,34 @@ export class CourseEntity extends Entity<CourseProps> {
     return this.props.createdAt;
   }
 
+  get code(): string {
+    return this.props.code;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  private set code(value: string) {
+    this.props.code = value;
+  }
+
+  private set name(value: string) {
+    this.props.name = value;
+  }
+
+  private set createdAt(value: Date) {
+    this.props.createdAt = value;
+  }
+
+  get updatedAt(): Date | undefined {
+    return this.props.updatedAt;
+  }
+
+  private set updatedAt(value: Date) {
+    this.props.updatedAt = value;
+  }
+
   static validate(props: CourseProps) {
     const validator = CourseValidatorFactory.create();
     const isValid = validator.validate(props);
