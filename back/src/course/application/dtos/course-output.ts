ea3b741d1c9d2dfb5fae8cd552e50469a867a5bf
davi <davi.araujo@onfly.com.br>
2025-03-30 11:33:23 -0300

    add course presenter

--- back/src/course/application/dtos/course-output.ts
+++ back/src/course/application/dtos/course-output.ts
@@ -1,6 +1,12 @@
 import { CourseEntity } from '@/course/domain/entities/course.entity';
 
-export type CourseOutput = {}
+export type CourseOutput = {
+  id: string;
+  name: string;
+  code: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+};
 
 export class CourseOutputMapper {
   static toOutput(entity: CourseEntity): CourseOutput {
--- back/src/course/infrastructure/presenters/course.presenter.ts
+++ back/src/course/infrastructure/presenters/course.presenter.ts
@@ -1,9 +1,34 @@
 import { CourseOutput } from '@/course/application/dtos/course-output';
 import { CollectionPresenter } from '@/shared/infrastructure/presenters/collection.presenter';
 import { ListCoursesUsecase } from '@/course/application/usecases/list-course.usecase';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class CoursePresenter {
-  constructor(output: CourseOutput) { }
+  @ApiProperty({ description: 'The id of course' })
+  id: string;
+
+  @ApiProperty({ description: 'The name of the course' })
+  name: string;
+
+  @ApiProperty({ description: 'The code of the course' })
+  code: string;
+
+  @ApiProperty({ description: 'The date when the course was created' })
+  @Transform(({ value }: { value: Date }) => value.toISOString())
+  createdAt: Date;
+
+  @ApiProperty({ description: 'The date when the course was updated' })
+  @Transform(({ value }: { value: Date }) => value.toISOString())
+  updatedAt: Date;
+
+  constructor(output: CourseOutput) {
+    this.id = output.id;
+    this.name = output.name;
+    this.code = output.code;
+    this.createdAt = output.createdAt;
+    this.updatedAt = output.updatedAt;
+  }
 }
 
 export class CourseCollectionPresenter extends CollectionPresenter {
