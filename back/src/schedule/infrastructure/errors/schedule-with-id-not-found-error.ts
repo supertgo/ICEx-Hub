b521ee9032b96486a118d94f4179f5a2985fe616
supertgo <thiagoroberto2003@gmail.com>
2025-03-31 18:13:24 -0300

    refac(schedule): rename schedule not found class

--- back/src/schedule/infrastructure/errors/schedule-with-id-not-found-error.ts
+++ back/src/schedule/infrastructure/errors/schedule-with-id-not-found-error.ts
@@ -1,4 +1,4 @@
-export class scheduleWithIdNotFoundError extends Error {
+export class ScheduleWithIdNotFoundError extends Error {
   constructor(public id: string) {
     super(`schedule having id ${id} not found`);
     this.name = 'scheduleWithIdNotFoundError';
