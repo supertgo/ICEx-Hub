87132a601d08df137bd8901f37a2adca5454670e
Lauraolv <oliveiraribeirolaura@icloud.com>
2025-03-31 14:29:02 -0300

    card#16: teste no front com vitest

---
+++ front/src/components/__tests__/StatusCircle.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+import StatusCircle from '../StatusCircle.vue'
+
+
+test('StatusCircle component shoudl be defined', () => {
+    expect(StatusCircle).toBeDefined()
+})
