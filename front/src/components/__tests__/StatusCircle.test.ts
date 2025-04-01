be7ce6a82c85c95dae56ddd1dcda48f503345fcd
Lauraolv <oliveiraribeirolaura@icloud.com>
2025-04-01 17:55:45 -0300

    Correção dos testes StatusCircle

--- front/src/components/__tests__/StatusCircle.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest'
-import StatusCircle from '../StatusCircle.vue'
-
-
-test('StatusCircle component shoudl be defined', () => {
-    expect(StatusCircle).toBeDefined()
-})
