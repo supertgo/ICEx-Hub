c4fb0758495d7bff979e617d72666331c2c56abf
supertgo <thiagoroberto2003@gmail.com>
2025-04-02 08:34:54 -0300

    feat(schedule): add list schedule update

--- back/src/schedule/infrastructure/dtos/list-schedule.dto.ts
+++ back/src/schedule/infrastructure/dtos/list-schedule.dto.ts
@@ -1,10 +1,35 @@
-import { UpdateScheduleUsecase } from '@/schedule/application/usecases/update-schedule.usecase';
-import { IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsIn, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ListSchedulesUsecase } from '@/schedule/application/usecases/list-schedule.usecase';
+import { SortOrderEnum } from '@/shared/domain/repositories/searchable-repository-contracts';
 
-export class UpdateScheduleDto implements Omit<UpdateScheduleUsecase.Input, 'id'> {
-  @ApiProperty({ description: 'The name of the schedule' })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
+export class ListSchedulesDto implements ListSchedulesUsecase.Input {
+  @ApiPropertyOptional({ description: 'The page number' })
+  @IsOptional()
+  page?: number;
+
+  @ApiPropertyOptional({ description: 'The number of items per page' })
+  @IsOptional()
+  perPage?: number;
+
+  @ApiPropertyOptional({
+    description: 'The field that should be used for sorting',
+    enum: ['createdAt'],
+  })
+  @IsOptional()
+  sort?: string | null;
+
+  @ApiPropertyOptional({
+    description: 'The sort direction',
+    enum: SortOrderEnum,
+  })
+  @IsOptional()
+  @IsIn([SortOrderEnum.ASC, SortOrderEnum.DESC])
+  sortDir?: SortOrderEnum | null;
+
+  @ApiPropertyOptional({
+    description: 'The filter to apply to the search',
+  })
+  @IsOptional()
+  filter?: string | null;
 }
--- back/src/schedule/infrastructure/dtos/update-schedule.dto.ts
+++ back/src/schedule/infrastructure/dtos/update-schedule.dto.ts
@@ -2,7 +2,9 @@ import { UpdateScheduleUsecase } from '@/schedule/application/usecases/update-sc
 import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateScheduleDto implements Omit<UpdateScheduleUsecase.Input, 'id'> {
+export class UpdateScheduleDto
+  implements Omit<UpdateScheduleUsecase.Input, 'id'>
+{
   @ApiProperty({ description: 'The name of the schedule' })
   @IsString()
   @IsNotEmpty()
