11f408381eefb1c8ab1a69cdf0a608d865bd7027
supertgo <thiagoroberto2003@gmail.com>
2025-04-02 08:32:51 -0300

    chore(schedule): add fake classroom at builder

--- back/src/schedule/domain/testing/helper/schedule-data-builder.ts
+++ back/src/schedule/domain/testing/helper/schedule-data-builder.ts
@@ -1,11 +1,13 @@
 import { faker } from '@faker-js/faker';
 import { ScheduleProps } from '@/schedule/domain/entities/schedule.entity';
 import { DayPatternEnum, TimeSlotEnum } from '../../schedule.constants';
+import { ClassroomFakeBuilder } from '@/classroom/domain/fake-builder/classroom-fake.builder';
 
 export function ScheduleDataBuilder(props: Partial<ScheduleProps>) {
   return {
     disciplineId: props.disciplineId || faker.string.uuid(),
     classroomId: props.classroomId || faker.string.uuid(),
+    classroom: props.classroom || ClassroomFakeBuilder.aCADClassroom().build(),
     dayPattern: props.dayPattern || DayPatternEnum.TUESDAY_THURSDAY,
     timeSlot: props.timeSlot || TimeSlotEnum.EVENING_1,
     createdAt: props.createdAt || new Date(),
