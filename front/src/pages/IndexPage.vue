a28e6943fa3eae05a0725b4766e183ec7f65fd17
supertgo <thiagoroberto2003@gmail.com>
2025-04-03 15:40:07 -0300

    feat(schdule page): retrive content from api

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -1,6 +1,7 @@
 <style lang="scss">
 @import '../css/index.page.scss';
-@import '../css/quasar.variables.scss';</style>
+@import '../css/quasar.variables.scss';
+</style>
 
 <template>
   <div :class="['welcome-text-and-status-circle', 'q-pa-md']">
@@ -14,14 +15,29 @@
     </div>
   </div>
   <div :class="['q-pa-md', 'table']">
-    <q-table card-class="bg-grey-4 text-black" table-header-class="bg-blue-10 text-white font-bold text-uppercase"
-     flat bordered :rows="rows" :columns="columns" row-key="name" hide-bottom />
+    <q-table
+      card-class="bg-grey-4 text-black"
+      table-header-class="bg-blue-10 text-white font-bold text-uppercase"
+      flat
+      bordered
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      hide-bottom
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { QTableColumn } from 'quasar';
 import StatusCircle from 'src/components/StatusCircle.vue';
+import { useScheduleStore } from 'src/stores/schedule';
+import { type Schedule } from 'src/types/schedule';
+import { onMounted, ref } from 'vue';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const rows = ref<any[]>([]);
+const scheduleStore = useScheduleStore();
 
 const columns: QTableColumn[] = [
   {
@@ -31,67 +47,41 @@ const columns: QTableColumn[] = [
     align: 'center',
     field: (row: { name: string }) => row.name,
     format: (val: string) => `${val}`,
-    classes:'discipline-column',
+    classes: 'discipline-column',
   },
   { name: 'code', align: 'center', label: 'Código', field: 'code' },
-  { name: 'class', align: 'center', label: 'Turma', field: 'class' },
+  //{ name: 'class', align: 'center', label: 'Turma', field: 'class' },
   { name: 'start', align: 'center', label: 'Início', field: 'start' },
   { name: 'end', align: 'center', label: 'Fim', field: 'end' },
   { name: 'days', align: 'center', label: 'Dias', field: 'days' },
   { name: 'unit', align: 'center', label: 'Unidade', field: 'unit' },
   { name: 'classroom', align: 'center', label: 'Sala', field: 'classroom' },
-  { name: 'direction', align: 'center', label: 'Como chegar', field: 'direction' },
+  {
+    name: 'direction',
+    align: 'center',
+    label: 'Como chegar',
+    field: 'direction',
+  },
   { name: 'status', align: 'center', label: 'Status', field: 'status' },
 ];
 
-const rows = [
-  {
-    name: 'Algoritmos I',
-    code: 'DCC206',
-    class: 'TN',
-    start: '14:55',
-    end: '16:35',
-    days: 'Ter-Qui',
-    unit: 'CAD 3',
-    classroom: '201',
-    direction: 'Ver Mapa',
-    status: false,
-  },
-  {
-    name: 'Algoritmos I',
-    code: 'DCC206',
-    class: 'TN1',
-    start: '19:00',
-    end: '20:40',
-    days: 'Ter-Qui',
-    unit: 'CAD 3',
-    classroom: '201',
-    direction: 'Ver Mapa',
-    status: false,
-  },
-  {
-    name: 'Organ. e Tratamento da Informação',
-    code: 'OTI071',
-    class: 'TZ',
-    start: '17:00',
-    end: '18:40',
-    days: 'Seg-Qua',
-    unit: 'CAD 3',
-    classroom: '412',
-    direction: 'Ver Mapa',
-    status: false,
-  },
-  {
-    name: 'Estrutura de Dados',
-    code: 'DCC205',
-    class: 'TW',
-    start: '19:00',
-    end: '20:40',
-    days: 'Ter-Qui',
-    unit: 'CAD 3',
-    classroom: 'LAICO B307',
-    direction: 'Ver Mapa',
-    status: false,
-  },
-];
+onMounted(async () => {
+  try {
+    const result = await scheduleStore.listSchedules();
+    rows.value = result.data.map((item: Schedule) => ({
+      name: item.discipline?.name || 'N/A',
+      code: item.discipline?.code || 'N/A',
+      //class: item.classIdentifier || 'N/A',
+      start: item.timeSlot || 'N/A',
+      end: item.timeSlot || 'N/A',
+      days: item.dayPattern || 'N/A',
+      unit: item.classroom?.building || 'N/A',
+      classroom: item.classroom?.name || 'N/A',
+      direction: 'Ver Mapa',
+      status: false,
+    }));
+  } catch (error) {
+    console.error('Error loading schedules:', error);
+  }
+});
 </script>
