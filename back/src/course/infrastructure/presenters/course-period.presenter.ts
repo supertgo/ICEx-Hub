937040e8744273cc66349c1d0e92da2a1377d2bb
daviporto <portodaviporto@gmail.com>
2025-04-03 19:50:21 -0300

    add course period presenter and its tests

---
+++ back/src/course/infrastructure/presenters/course-period.presenter.ts
@@ -0,0 +1,37 @@
+import { CollectionPresenter } from '@/shared/infrastructure/presenters/collection.presenter';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { CoursePeriodOutput } from '@/course/application/dtos/course-period-output';
+import { ListCoursePeriodUsecase } from '@/course/application/usecases/list-course-period.usecase';
+
+export class CoursePeriodPresenter {
+  @ApiProperty({ description: 'The id of course' })
+  id: string;
+
+  @ApiProperty({ description: 'The name of the course' })
+  name: string;
+  @ApiProperty({ description: 'The date when the course was created' })
+  @Transform(({ value }: { value: Date }) => value.toISOString())
+  createdAt: Date;
+
+  @ApiProperty({ description: 'The date when the course was updated' })
+  @Transform(({ value }: { value: Date }) => value.toISOString())
+  updatedAt: Date;
+
+  constructor(output: CoursePeriodOutput) {
+    this.id = output.id;
+    this.name = output.name;
+    this.createdAt = output.createdAt;
+    this.updatedAt = output.updatedAt;
+  }
+}
+
+export class CourseCollectionPresenter extends CollectionPresenter {
+  data: CoursePeriodPresenter[];
+
+  constructor(output: ListCoursePeriodUsecase.Output) {
+    const { items, ...pagination } = output;
+    super(pagination);
+    this.data = items.map((item) => new CoursePeriodPresenter(item));
+  }
+}
