8adeba11abf02cd15a35875ca90e84f6a5e51dd9
daviporto <portodaviporto@gmail.com>
2025-04-05 10:54:55 -0300

    test add course to not found error filter

--- back/src/shared/infrastructure/exception-filters/not-found-error/__test__/e2e/not-found-error.filter.e2e-spec.ts
+++ back/src/shared/infrastructure/exception-filters/not-found-error/__test__/e2e/not-found-error.filter.e2e-spec.ts
@@ -5,24 +5,38 @@ import request from 'supertest';
 import { UserWithIdNotFoundError } from '@/user/infrastructure/errors/user-with-id-not-found-error';
 import { NotFoundErrorFilter } from '@/shared/infrastructure/exception-filters/not-found-error/not-found-error.filter';
 import { UserWithEmailNotFoundError } from '@/user/domain/errors/user-with-email-not-found-error';
+import { CourseWithIdNotFoundError } from '@/course/infrastructure/Errors/course-with-id-not-found-error';
+import { CoursePeriodWithIdNotFoundError } from '@/course/infrastructure/Errors/course-period-with-id-not-found-error';
 
 const id = faker.string.uuid();
 const email = faker.internet.email();
+const courseId = faker.string.uuid();
+const coursePeriodId = faker.string.uuid();
 
 @Controller('/stub')
 class StubController {
-  @Get('/id')
-  idException() {
+  @Get('/user-id')
+  userIdException() {
     throw new UserWithIdNotFoundError(id);
   }
 
-  @Get('/email')
-  emailException() {
+  @Get('/user-email')
+  userEmailException() {
     throw new UserWithEmailNotFoundError(email);
+  }
+
+  @Get('/course-id')
+  courseIdException() {
+    throw new CourseWithIdNotFoundError(courseId);
+  }
+
+  @Get('/course-period-id')
+  coursePeriodIdException() {
+    throw new CoursePeriodWithIdNotFoundError(coursePeriodId);
   }
 }
 
-describe('ConflictErrorFilter', () => {
+describe('NotFoundErrorFilter', () => {
   let app: INestApplication;
   let module: TestingModule;
 
@@ -44,9 +58,9 @@ describe('ConflictErrorFilter', () => {
     expect(new NotFoundErrorFilter()).toBeDefined();
   });
 
-  it('should catch id error correctly', async () => {
+  it('should catch user id error correctly', async () => {
     await request(app.getHttpServer())
-      .get('/stub/id')
+      .get('/stub/user-id')
       .expect(404)
       .expect({
         statusCode: 404,
@@ -55,14 +69,36 @@ describe('ConflictErrorFilter', () => {
       });
   });
 
-  it('should catch email error correctly', async () => {
+  it('should catch user email error correctly', async () => {
     await request(app.getHttpServer())
-      .get('/stub/email')
+      .get('/stub/user-email')
       .expect(404)
       .expect({
         statusCode: 404,
         error: 'Not Found',
         message: `User with email ${email} not found`,
+      });
+  });
+
+  it('should catch course id error correctly', async () => {
+    await request(app.getHttpServer())
+      .get('/stub/course-id')
+      .expect(404)
+      .expect({
+        statusCode: 404,
+        error: 'Not Found',
+        message: `Course having id ${courseId} not found`,
+      });
+  });
+
+  it('should catch course period id error correctly', async () => {
+    await request(app.getHttpServer())
+      .get('/stub/course-period-id')
+      .expect(404)
+      .expect({
+        statusCode: 404,
+        error: 'Not Found',
+        message: `Course period having id ${coursePeriodId} not found`,
       });
   });
 });
