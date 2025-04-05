fd6c62f5b47467591ffe2dc0dbd18f78988822ab
daviporto <portodaviporto@gmail.com>
2025-04-05 11:51:32 -0300

    fix signup tests

--- back/src/user/application/usecases/__test__/unit/signup.usecase.spec.ts
+++ back/src/user/application/usecases/__test__/unit/signup.usecase.spec.ts
@@ -3,33 +3,96 @@ import { UserInMemoryRepository } from '@/user/infrastructure/database/in-memory
 import { HashProvider } from '@/shared/application/providers/hash-provider';
 import { BcryptjsHashProvider } from '@/user/infrastructure/providers/hash-provider/bcryptjs-hash.provider';
 import { BadRequestError } from '@/shared/application/errors/bad-request-error';
+import { CourseInMemoryRepository } from '@/course/infrastructure/database/in-memory/repositories/course-in-memory.repository';
+import { CoursePeriodInMemoryRepository } from '@/course/infrastructure/database/in-memory/repositories/course-period-in-memory.repository';
+import { faker } from '@faker-js/faker';
+import { CoursePeriodEntity } from '@/course/domain/entities/course-period.entity';
+import { CourseEntity } from '@/course/domain/entities/course.entity';
 
 describe('Signup use case test', () => {
   let sut: SignupUsecase.UseCase;
   let repository: UserInMemoryRepository;
   let hashProvider: HashProvider;
+  let coursePeriodRepository: CoursePeriodInMemoryRepository;
+  let courseRepository: CourseInMemoryRepository;
+  let course: CourseEntity;
+  let coursePeriod: CoursePeriodEntity;
 
   beforeEach(() => {
     repository = new UserInMemoryRepository();
     hashProvider = new BcryptjsHashProvider();
-    sut = new SignupUsecase.UseCase(repository, hashProvider);
+    courseRepository = new CourseInMemoryRepository();
+    coursePeriodRepository = new CoursePeriodInMemoryRepository();
+    sut = new SignupUsecase.UseCase(
+      repository,
+      hashProvider,
+      courseRepository,
+      coursePeriodRepository,
+    );
+
+    coursePeriod = CoursePeriodEntity.fake().aCoursePeriod().build();
+    coursePeriodRepository.insert(coursePeriod);
+
+    course = CourseEntity.fake().aCourse().build();
+    courseRepository.insert(course);
   });
 
   describe('Bad request errors', () => {
     it('should throw a BadRequestError if name is not provided', async () => {
-      const input = { name: '', email: 'test@example.com', password: '123456' };
+      const input = {
+        name: '',
+        email: 'test@example.com',
+        password: '123456',
+        courseId: faker.string.uuid(),
+        coursePeriodId: faker.string.uuid(),
+      };
 
       await expect(sut.execute(input)).rejects.toThrow(BadRequestError);
     });
 
     it('should throw a BadRequestError if email is not provided', async () => {
-      const input = { name: 'Test', email: '', password: '123456' };
+      const input = {
+        name: 'Test',
+        email: '',
+        password: '123456',
+        courseId: faker.string.uuid(),
+        coursePeriodId: faker.string.uuid(),
+      };
 
       await expect(sut.execute(input)).rejects.toThrow(BadRequestError);
     });
 
     it('should throw a BadRequestError if password is not provided', async () => {
-      const input = { name: 'Test', email: 'test@example.com', password: '' };
+      const input = {
+        name: 'Test',
+        email: 'test@example.com',
+        password: '',
+        courseId: faker.string.uuid(),
+        coursePeriodId: faker.string.uuid(),
+      };
+
+      await expect(sut.execute(input)).rejects.toThrow(BadRequestError);
+    });
+
+    it('should throw a BadRequestError if courseId is not provided', async () => {
+      const input = {
+        name: 'Test',
+        email: 'test@example.com',
+        password: 'asdasdsad',
+        courseId: '',
+        coursePeriodId: faker.string.uuid(),
+      };
+
+      await expect(sut.execute(input)).rejects.toThrow(BadRequestError);
+    });
+    it('should throw a BadRequestError if coursePeriodId is not provided', async () => {
+      const input = {
+        name: 'Test',
+        email: 'test@example.com',
+        password: 'adsasd',
+        coursePeriodId: '',
+        courseId: faker.string.uuid(),
+      };
 
       await expect(sut.execute(input)).rejects.toThrow(BadRequestError);
     });
@@ -40,6 +103,8 @@ describe('Signup use case test', () => {
       name: 'Test',
       email: 'test@example.com',
       password: '123456',
+      courseId: course.id,
+      coursePeriodId: coursePeriod.id,
     };
 
     const spyAssureEmail = jest.spyOn(
@@ -52,11 +117,48 @@ describe('Signup use case test', () => {
     expect(spyAssureEmail).toHaveBeenCalledWith(input.email);
   });
 
+  it('should ensure course exists before signup', async () => {
+    const input = {
+      name: 'Test',
+      email: 'test@example.com',
+      password: '123456',
+      courseId: course.id,
+      coursePeriodId: coursePeriod.id,
+    };
+
+    const spyCourseExists = jest.spyOn(courseRepository, 'assureCourseExists');
+
+    await sut.execute(input);
+
+    expect(spyCourseExists).toHaveBeenCalledWith(input.courseId);
+  });
+
+  it('should ensure course period exists before signup', async () => {
+    const input = {
+      name: 'Test',
+      email: 'test@example.com',
+      password: '123456',
+      courseId: course.id,
+      coursePeriodId: coursePeriod.id,
+    };
+
+    const spyCoursePeriodExists = jest.spyOn(
+      coursePeriodRepository,
+      'assureCoursePeriodExists',
+    );
+
+    await sut.execute(input);
+
+    expect(spyCoursePeriodExists).toHaveBeenCalledWith(input.coursePeriodId);
+  });
+
   it('should hash the password before storing the user', async () => {
     const input = {
       name: 'Test',
       email: 'test@example.com',
       password: '123456',
+      courseId: course.id,
+      coursePeriodId: coursePeriod.id,
     };
 
     const spyGenerateHash = jest.spyOn(hashProvider, 'generateHash');
@@ -71,6 +173,8 @@ describe('Signup use case test', () => {
       name: 'Test',
       email: 'test@example.com',
       password: '123456',
+      courseId: course.id,
+      coursePeriodId: coursePeriod.id,
     };
 
     const spyInsert = jest.spyOn(repository, 'insert');
@@ -93,6 +197,8 @@ describe('Signup use case test', () => {
       name: 'Test',
       email: 'test@example.com',
       password: '123456',
+      courseId: course.id,
+      coursePeriodId: coursePeriod.id,
     };
 
     const result = await sut.execute(input);
