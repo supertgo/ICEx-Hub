9fb41b89dccabf6f5bf92dfc009649413b9fe3f5
daviporto <portodaviporto@gmail.com>
2025-04-06 20:14:30 -0300

    add coursePeriod types

---
+++ front/src/types/coursePeriod.d.ts
@@ -0,0 +1,10 @@
+export interface CoursePeriod {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface AutocompleteCoursePeriodData {
+  autocomplete: string;
+}
