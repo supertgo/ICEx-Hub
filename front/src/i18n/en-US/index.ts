7a7839689e728cfffb2c16b5cb109f9a3eea656b
daviporto <portodaviporto@gmail.com>
2025-04-06 20:14:01 -0300

    add coursePeriod translation file to i18n

--- front/src/i18n/en-US/index.ts
+++ front/src/i18n/en-US/index.ts
@@ -1,9 +1,11 @@
 import auth from './auth.json'
 import common from './common.json'
 import course from './course.json'
+import coursePeriod from './coursePeriod.json'
 
 export default {
   auth,
   common,
-  course
+  course,
+  coursePeriod
 }
