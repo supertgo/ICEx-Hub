0a8e746e6422c94bc41162b2aca43e60ad2495eb
supertgo <thiagoroberto2003@gmail.com>
2025-04-08 15:47:26 -0300

    test: add todo taks

---
+++ front/src/utils/schedule/__tests__/table.spec.ts
@@ -0,0 +1,9 @@
+//TODO Arthur & Laura > The table needs a method that map for every
+//timeSlot, the start and end hour. Ex: MORNING_1 -> (start: 7:30, end: 9:10).
+// You should use this method with the scheduleDataToOutput
+
+//TODO Arthur & Laura -> The table also needs a method that gets the resoult of the
+// method above with the dayPattern and checks if the current day and hour matches with some schedule.
+// Should return true if matches or false otherwise. This method should be used on
+// the last column('status'), so true will render the green ball and false will render
+// the red one.
