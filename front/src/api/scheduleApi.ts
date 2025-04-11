4e823da04a54596e1e9185cfa1041362a5c2a4bc
supertgo <thiagoroberto2003@gmail.com>
2025-04-11 19:20:42 -0300

    feat: add courseId and cousePeriodId params

--- front/src/api/scheduleApi.ts
+++ front/src/api/scheduleApi.ts
@@ -1,8 +1,22 @@
 import { api } from 'boot/axios';
-import { type ScheduleData } from 'src/types/schedule';
+import { type ListScheduleParams, type ScheduleData } from 'src/types/schedule';
 
-export const listSchedules = async (): Promise<ScheduleData> => {
-  const response = await api.get('/schedule');
+export const listSchedules = async ({
+  courseId,
+  coursePeriodId,
+}: ListScheduleParams): Promise<ScheduleData> => {
+  const url = new URL('http://localhost:3000/schedule');
+  const params = new URLSearchParams(url.search);
+
+  if (courseId) {
+    params.set('courseId', courseId);
+  }
+
+  if (coursePeriodId) {
+    params.set('coursePeriodId', coursePeriodId);
+  }
+
+  const response = await api.get(`/schedule?${params.toString()}`);
 
   return response.data;
 };
