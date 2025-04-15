5775a30662b6f1275df19104a562a487bb7abdf2
supertgo <thiagoroberto2003@gmail.com>
2025-04-15 04:52:53 -0300

    feat: add enums in schedule table file

--- front/src/utils/schedule/table.ts
+++ front/src/utils/schedule/table.ts
@@ -5,6 +5,28 @@ import {
   type ScheduleRows,
 } from 'src/types/schedule';
 
+export enum TimeSlotEnum {
+  MORNING_1 = 'MORNING_1',
+  MORNING_2 = 'MORNING_2',
+  MORNING_3 = 'MORNING_3',
+  AFTERNOON_1 = 'AFTERNOON_1',
+  AFTERNOON_2 = 'AFTERNOON_2',
+  EVENING_1 = 'EVENING_1',
+  EVENING_2 = 'EVENING_2',
+  EVENING_3 = 'EVENING_3',
+}
+
+export enum DayPatternEnum {
+  MONDAY = 'MONDAY',
+  TUESDAY = 'TUESDAY',
+  WEDNESDAY = 'WEDNESDAY',
+  THURSDAY = 'THURSDAY',
+  FRIDAY = 'FRIDAY',
+  SATURDAY = 'SATURDAY',
+  MONDAY_WEDNESDAY = 'MONDAY_WEDNESDAY',
+  TUESDAY_THURSDAY = 'TUESDAY_THURSDAY',
+}
+
 //TODO Arthur & Laura -> schduleDataToOutput(create test for this method on table.spec.ts)
 export function scheduleDataToOutput(schedules: ScheduleData) {
   return schedules.data.map(
