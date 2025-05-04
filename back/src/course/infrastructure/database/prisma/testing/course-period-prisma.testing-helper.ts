3f36b96ecd6cf68ade92da95ad620e430380d0a2
daviporto <portodaviporto@gmail.com>
2025-05-04 08:03:11 -0300

    fix course-period testing helper to use course testing helper

--- back/src/course/infrastructure/database/prisma/testing/course-period-prisma.testing-helper.ts
+++ back/src/course/infrastructure/database/prisma/testing/course-period-prisma.testing-helper.ts
@@ -1,16 +1,14 @@
 import { CoursePeriod, PrismaClient } from '@prisma/client';
-import { CourseDataBuilder } from '@/user/domain/testing/helper/course-data-builder';
 import { CoursePeriodProps } from '@/course/domain/entities/course-period.entity';
 import { CoursePeriodDataBuilder } from '@/user/domain/testing/helper/course-period-data-builder';
+import { CoursePrismaTestingHelper } from '@/course/infrastructure/database/prisma/testing/course-prisma.testing-helper';
 
 export class CoursePeriodPrismaTestingHelper {
   static async createCoursePeriod(
     prisma: PrismaClient,
     props: Partial<CoursePeriodProps> = {},
   ): Promise<CoursePeriod> {
-    const course = await prisma.course.create({
-      data: CourseDataBuilder(props),
-    });
+    const course = await CoursePrismaTestingHelper.createCourse(prisma);
 
     return prisma.coursePeriod.create({
       data: {
